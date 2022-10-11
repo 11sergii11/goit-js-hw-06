76881a8dc988c240a3a934835437f804581f67b4
11sergii11 <104842080+11sergii11@users.noreply.github.com>
2022-10-11 13:11:13 +0200

    2

--- js/task-08.js
+++ js/task-08.js
@@ -1,24 +1,25 @@
-const formRef = document.querySelector(".login-form");
+const formEl = document.querySelector('.login-form');
 
-formRef.addEventListener("submit", onFormSubmit);
+formEl.addEventListener('submit', onFormSubmit);
 
 function onFormSubmit(event) {
-  event.preventDefault();
-  const formElements = event.currentTarget.elements;
-  const email = formElements.email.value;
-  const password = formElements.password.value;
+    event.preventDefault();
 
-  if (onEmptyString(email) || onEmptyString(password)) {
-    return alert("все поля должны быть заполнены");
-  }
-  const formData = {
-    email,
-    password,
-  };
-  formRef.reset();
-  console.log(formData);
-}
+    const formElements = event.currentTarget.elements;
+    const email = formElements.email.value;
+    const password = formElements.password.value;
 
-function onEmptyString(str) {
-  return !str || 0 === str.length;
-}
+    if (formElements.email.value === "" || formElements.password.value === "") {
+        return console.log("Please fill in all the fields!");
+    }
+    
+    const formData = {
+        email,
+        password,
+    };
+
+    console.log(formData);
+
+    event.currentTarget.reset();
+
+};
